42dbd004996ff2a942a0d1c862791dd0b22ae06a
CyberWorrior <ayush13things@gmail.com>
2020-04-20 12:40:06 +0530

    Battery Percentage

---
+++ batteryIndicator/batteryIndicator.ino
@@ -0,0 +1,16 @@
+
+ 
+  
+
+void setup() 
+{
+Serial.begin(9600);
+}
+
+void loop()
+{
+  int val = analogRead(0); 
+  float volts = val*(5.0/1023.0); 
+  Serial.println(volts);
+  delay(3000);
+  }
